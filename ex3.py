9801bdabcc5b1c88a9228b592a011da5f6988375
Donelle Harris <donelle.harris@gmail.com>
2021-01-28 20:17:40 -0600

    Learn Python the Hard Way exercises 1 - 5

---
+++ ex3.py
@@ -0,0 +1,26 @@
+#prints statement in quotations
+print("I will now count my chickens:")
+
+#prints "Hens" then does the math
+print("Hens", 25 + 30/6)
+#prints "Roosters" then does the math
+print("Roosters", 100 - 25 * 3 % 4)
+#prints statement in quotes
+print("Now I will count the eggs:")
+#does the math and prints the answer
+print( 3 + 2 + 1 - 5 + 4 % 2 -1 / 4 + 6)
+#prints the statement in quotes
+print("Is it true that  3 + 2 < 5 - 7?")
+#does the math and prints the answer
+print(3 + 2 < 5 - 7) 
+
+print("What is 3 + 2?", 3 + 2)
+print("What is 5 - 7", 5 - 7)
+
+print("Oh, that's why it's False.")
+
+print("How about some more.")
+
+print("Is it greater?", 5 > -2)
+print("Is is it greater or equal?", 5 >= -2)
+print("Is is it less or equal?", 5<= -2)
---
+++ ex5.py
@@ -0,0 +1,14 @@
+myName = 'Donelle Harris'
+myAge = 35 #Total lie
+myHeight = 66 #inches
+myWeight = 'none of your business'
+myEyes = 'green'
+myTeeth = 'straight'
+myHair = 'brown'
+
+print(f"Let's talk about {myName}")
+print(f"She's {myHeight} inches tall.")
+print(f"Her weight is {myWeight}.")
+print(":P")
+print(f"She's got {myEyes} eyes and {myHair} hair.")
+print(f"Her teeth are really {myTeeth} thanks to braces.")
